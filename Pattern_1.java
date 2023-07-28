884f1b799c7ed76a14e991c6ced339213c95f603
Madhuri122002 <94678055+Madhuri122002@users.noreply.github.com>
2023-07-28 18:08:53 +0530

    Half Pyramid

--- Pattern_1.java
+++ Pattern_1.java
@@ -3,9 +3,7 @@ public class Pattern_1 {
         for(int i=0;i<6;i++)
         {
             for(int j=0;j<=i;j++)
-            {
                 System.out.print("*");
-            }
             System.out.println();
         }
     }
